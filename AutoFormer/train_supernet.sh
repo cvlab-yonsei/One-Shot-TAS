724e9eacc6f9980d0ec6fae6db7c3b8638102a80
dlguswn3659 <dlguswn3659@naver.com>
2024-10-08 14:24:28 +0900

    [add] using full validation data for evaluating 2500 subnets

--- AutoFormer/train_supernet.sh
+++ AutoFormer/train_supernet.sh
@@ -2,37 +2,37 @@
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch100-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch100-top-k(full).log' \
 --resume './greedyTAS/checkpoint-4.pth' --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch200-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch200-top-k(full).log' \
 --resume './greedyTAS/checkpoint-9.pth' --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch300-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch300-top-k(full).log' \
 --resume './greedyTAS/checkpoint-14.pth' --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch400-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch400-top-k(full).log' \
 --resume './greedyTAS/checkpoint-19.pth' --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch500-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch500-top-k(full).log' \
 --resume './greedyTAS/checkpoint-24.pth' --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch0-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch0-top-k(full).log' \
 --output /OUTPUT_PATH --batch-size 128 
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train.py --data-path '/data' --gp \
 --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 500 --warmup-epochs 20 \
---log-file-path './greedyTAS/greedyTAS-epoch20-top-k(1024).log' \
+--log-file-path './greedyTAS/greedyTAS-epoch20-top-k(full).log' \
 --resume './greedyTAS/checkpoint-0.pth' --output /OUTPUT_PATH --batch-size 128 
 
 
