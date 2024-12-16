ba91168d19204e8c84a6c8d3b8482f716d8c70ef
dlguswn3659 <dlguswn3659@naver.com>
2024-12-16 17:35:29 +0900

    [add] supernet_train_MLP.py

--- AutoFormer_original_greedy/supernet_train_MLP.py
+++ AutoFormer_original_greedy/supernet_train_MLP.py
@@ -19,7 +19,6 @@ from lib.samplers import RASampler
 from lib import utils
 from lib.config import cfg, update_config_from_file
 from model.supernet_transformer import Vision_TransformerSuper
-
 import sys
 import warnings
 
