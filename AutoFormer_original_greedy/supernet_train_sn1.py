1d82b5415cdbe935ccecca34f41ef92aec26f1dc
dlguswn3659 <dlguswn3659@naver.com>
2025-01-09 10:08:54 +0900

    [fix] mixup, cutmix setting original -> pre-nas

--- AutoFormer_original_greedy/supernet_train_sn1.py
+++ AutoFormer_original_greedy/supernet_train_sn1.py
@@ -149,35 +149,35 @@ def get_args_parser():
 
     # * Mixup params
     # original
-    parser.add_argument('--mixup', type=float, default=0.8,
-                        help='mixup alpha, mixup enabled if > 0. (default: 0.8)')
-    parser.add_argument('--cutmix', type=float, default=1.0,
-                        help='cutmix alpha, cutmix enabled if > 0. (default: 1.0)')
-    parser.add_argument('--cutmix-minmax', type=float, nargs='+', default=None,
-                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
-    parser.add_argument('--mixup-prob', type=float, default=1.0,
-                        help='Probability of performing mixup or cutmix when either/both is enabled')
-    parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
-                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
-    parser.add_argument('--mixup-mode', type=str, default='batch',
-                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
-    
-    # pre-nas aug
-    # parser.add_argument('--mixup', type=float, default=0.0,
-    #                     help='mixup alpha, mixup disabled if 0. (default: 0.0)')
-    # parser.add_argument('--cutmix', type=float, default=0.0,
-    #                     help='cutmix alpha, cutmix disabled if 0. (default: 0.0)')
-    # parser.add_argument('--mixup-switch-prob', type=float, default=0.0,
-    #                     help='Probability of switching to cutmix when both mixup and cutmix enabled (default: 0.0)')
-
+    # parser.add_argument('--mixup', type=float, default=0.8,
+    #                     help='mixup alpha, mixup enabled if > 0. (default: 0.8)')
+    # parser.add_argument('--cutmix', type=float, default=1.0,
+    #                     help='cutmix alpha, cutmix enabled if > 0. (default: 1.0)')
     # parser.add_argument('--cutmix-minmax', type=float, nargs='+', default=None,
     #                     help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
     # parser.add_argument('--mixup-prob', type=float, default=1.0,
     #                     help='Probability of performing mixup or cutmix when either/both is enabled')
-    # # parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
-    # #                     help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    # parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
+    #                     help='Probability of switching to cutmix when both mixup and cutmix enabled')
     # parser.add_argument('--mixup-mode', type=str, default='batch',
     #                     help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+    
+    # pre-nas aug
+    parser.add_argument('--mixup', type=float, default=0.0,
+                        help='mixup alpha, mixup disabled if 0. (default: 0.0)')
+    parser.add_argument('--cutmix', type=float, default=0.0,
+                        help='cutmix alpha, cutmix disabled if 0. (default: 0.0)')
+    parser.add_argument('--mixup-switch-prob', type=float, default=0.0,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled (default: 0.0)')
+
+    parser.add_argument('--cutmix-minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup-prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    # parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
+    #                     help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup-mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
 
     # Dataset parameters
     parser.add_argument('--data-path', default='./data/imagenet/', type=str,
