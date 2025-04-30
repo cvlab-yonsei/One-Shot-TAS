a6aa92d0fd89f5e00b649a1bfcaa6d85f5190d45
dlguswn3659 <dlguswn3659@naver.com>
2025-05-01 00:34:03 +0900

    [add] supernet_lr000001_feature_align_linear.log

--- AutoFormer_matryo_clone/train_supernet.sh
+++ AutoFormer_matryo_clone/train_supernet.sh
@@ -137,9 +137,9 @@
 # --save_checkpoint_path 'checkpoint-soft-freezeX-216-240-lr0001-regterm-0005-' --save_log_path './log/supernet_soft_freezeX_216_240_lr0001_regterm_0005.log'
 
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train_load_matryo_216_240.py --data-path '/dataset/ILSVRC2012' --gp \
---change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 40 --warmup-epochs 3 \
+--change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 40 --warmup-epochs 0 \
 --resume '/OUTPUT_PATH/checkpoint_original_check_only192_original_optimizer-epoch480-24.pth' --output /OUTPUT_PATH --batch-size 128 \
---save_checkpoint_path 'checkpoint-lr0001-feature-align-linear-' --save_log_path './log/supernet_lr0001_feature_align_linear.log'
+--save_checkpoint_path 'checkpoint-lr000001-feature-align-linear-' --save_log_path './log/supernet_lr000001_feature_align_linear.log'
 
 
 # python3 -m torch.distributed.launch --nproc_per_node=8 --use_env evolution_original.py --data-path '/dataset/ILSVRC2012' --gp \
