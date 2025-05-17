7b363a97162fb937d40ca1650a8222e459412c8e
dlguswn3659 <dlguswn3659@naver.com>
2025-05-18 01:33:54 +0900

    [add] supernet_lr0002_bc_no_a_40epoch.log

--- AutoFormer_matryo_clone/train_supernet.sh
+++ AutoFormer_matryo_clone/train_supernet.sh
@@ -176,10 +176,15 @@
 # --resume '/OUTPUT_PATH/checkpoint-lr0002-feature-align-all-check-grad-0.pth' --output /OUTPUT_PATH --batch-size 128 \
 # --save_checkpoint_path 'checkpoint-lr0002-feature-align-all-not-clip-check-grad-8-40-individual-lambda-' --save_log_path './log/supernet_lr0002_feature_align_all_not_clip_check_grad_8_40_individual_lambda.log'
 
+# python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train_load_matryo_216_240.py --data-path '/dataset/ILSVRC2012' --gp \
+# --change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 80 --warmup-epochs 0 \
+# --resume '/OUTPUT_PATH/checkpoint_original_check_only192_original_optimizer-epoch480-24.pth' --output /OUTPUT_PATH --batch-size 128 \
+# --save_checkpoint_path 'checkpoint-lr0002-BC-80epoch-' --save_log_path './log/supernet_lr0002_bc_80epoch.log'
+
 python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train_load_matryo_216_240.py --data-path '/dataset/ILSVRC2012' --gp \
---change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 80 --warmup-epochs 0 \
+--change_qk --relative_position --mode super --dist-eval --cfg ./experiments/supernet/supernet-T.yaml --epochs 40 --warmup-epochs 0 \
 --resume '/OUTPUT_PATH/checkpoint_original_check_only192_original_optimizer-epoch480-24.pth' --output /OUTPUT_PATH --batch-size 128 \
---save_checkpoint_path 'checkpoint-lr0002-BC-80epoch-' --save_log_path './log/supernet_lr0002_bc_80epoch.log'
+--save_checkpoint_path 'checkpoint-lr0002-BC-no-A-40epoch-' --save_log_path './log/supernet_lr0002_bc_no_a_40epoch.log'
 
 
 # python -m torch.distributed.launch --nproc_per_node=8 --use_env supernet_train_load_matryo_216_240_superinit.py --data-path '/dataset/ILSVRC2012' --gp \
